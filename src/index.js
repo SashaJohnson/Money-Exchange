89c4264f6ff5ff9d170e0528c0454fe12cbb279e
SashaJohnson <alexjohnson@yandex.ru>
2018-09-22 18:44:52 +0300

    implemented 'raw' version returning unrounded values

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    
+    var h = currency / 50;
+    
+    if (currency % 50 != 0) {
+        
+        var checkQuarters = currency % 50;
+
+        var q =  checkQuarters / 25;
+        
+        if (checkQuarters % 25 !=0) {
+            
+            var checkDimes = checkQuarters % 25;
+            
+            var d = checkDimes / 10;
+            
+            if (checkDimes %10 != 0) {
+                
+                var checkNickels = checkDimes % 10;
+                
+                var n = checkNickels / 5;
+                
+                if (checkNickels%5 != 0) {
+                    
+                    var p = checkNickels % 5;
+                    
+                }
+                
+            }
+        }
+    
+    }
+    
+    return (console.log(h, q, d, n, p));
 }
