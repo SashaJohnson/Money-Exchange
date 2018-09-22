86ae12a5f64fae738e35dc951da7c4373db0039a
SashaJohnson <alexjohnson@yandex.ru>
2018-09-22 18:56:19 +0300

    added rounding

--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
     
-    var h = currency / 50;
+    var h = Math.floor(currency / 50);
     
     if (currency % 50 != 0) {
         
         var checkQuarters = currency % 50;
 
-        var q =  checkQuarters / 25;
+        var q =  Math.floor(checkQuarters / 25);
         
-        if (checkQuarters % 25 !=0) {
+        if (checkQuarters % 25 != 0) {
             
             var checkDimes = checkQuarters % 25;
             
-            var d = checkDimes / 10;
+            var d = Math.floor(checkDimes / 10);
             
-            if (checkDimes %10 != 0) {
+            if (checkDimes % 10 != 0) {
                 
                 var checkNickels = checkDimes % 10;
                 
-                var n = checkNickels / 5;
+                var n = Math.floor(checkNickels / 5);
                 
-                if (checkNickels%5 != 0) {
+                if (checkNickels % 5 != 0) {
                     
                     var p = checkNickels % 5;
                     
                 }
-                
+              
             }
+            
         }
     
     }
     
     return (console.log(h, q, d, n, p));
+    
 }
