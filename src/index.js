ebc5c91fa402e9a3b51593e70a37a8c486b762ed
SashaJohnson <alexjohnson@yandex.ru>
2018-09-22 19:24:33 +0300

    added object construction

--- src/index.js
+++ src/index.js
@@ -1,39 +1,51 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
     
+    var response = {};
+    
     if (currency > 10000) {
         
-        var tooRich = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+        response = {error: "You are rich, my friend! We don't have so much coins for exchange"};
         
-        return tooRich;
+        return response;
         
     }
     
     else {
     
         var h = Math.floor(currency / 50);
+        
+        if (h > 0) { response["H"] = h;}
     
         if (currency % 50 != 0) {
         
             var checkQuarters = currency % 50;
 
             var q =  Math.floor(checkQuarters / 25);
+            
+            if (q > 0) { response["Q"] = q;}
         
             if (checkQuarters % 25 != 0) {
             
                 var checkDimes = checkQuarters % 25;
             
                 var d = Math.floor(checkDimes / 10);
+                
+                if (d > 0) { response["D"] = d;}
             
                 if (checkDimes % 10 != 0) {
                 
                     var checkNickels = checkDimes % 10;
                 
                     var n = Math.floor(checkNickels / 5);
+                    
+                    if (n > 0) { response["N"] = n;}
                 
                     if (checkNickels % 5 != 0) {
                     
                         var p = checkNickels % 5;
+                        
+                        if (p > 0) { response["P"] = p;}
                     
                 }
               
@@ -43,8 +55,8 @@ module.exports = function makeExchange(currency) {
     
     }
     
-    return (console.log(h, q, d, n, p));
-    
+    return response;
+   
 }
 
 }
